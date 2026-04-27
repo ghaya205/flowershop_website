171943956c5180f8681296590169ade1d549f0cd
ghaya <ghaya.bedoui.2000@gmail.com>
2026-04-27 14:34:47 +0100

    add user registration backend

---
+++ login.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "flowershop_db") or die ("Connection Failed: " . mysqli_connect_error());
+?>
--- signup.php
+++ signup.php
@@ -1,6 +1,39 @@
 <?php
+include 'login.php';
 
+$message = [];
+
+if (isset($_POST['submit-btn'])) {
+
+   $filter_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+   $name = mysqli_real_escape_string($conn, $filter_name);
+
+   $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+   $email = mysqli_real_escape_string($conn, $filter_email);
+
+   $filter_password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+   $password = mysqli_real_escape_string($conn, $filter_password);
+
+   $filter_cpassword = filter_var($_POST['c_password'], FILTER_SANITIZE_STRING);
+   $cpassword = mysqli_real_escape_string($conn, $filter_cpassword);
+
+   $select_user = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+
+   if (mysqli_num_rows($select_user) > 0) {
+      $message[] = 'user already exist!';
+   } else {
+      if ($password != $cpassword) {
+         $message[] = 'confirm password not matched!';
+      } else {
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password) VALUES('$name', '$email', '$password')") or die('query failed');
+         $message[] = 'registered successfully!';
+         header('location:login.php');
+         exit();
+      }
+   }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +43,31 @@
   <title>Sign up</title>
 </head>
 <body>
-  <section class = "form-container">
-    
-    <form action="register.php" method="POST">
-      <h3>Sign Up</h3>
-      <input type="text" name="username" placeholder="Username" required>
-      <input type="email" name="email" placeholder="Email" required>
-      <input type="password" name="password" placeholder="Password" required>
-     <input type="submit" name="submit-btn" class ="btn" value="Sign Up">
-      <p>Already have an account? <a href="login.php">Log in</a></p>
-    </form>
-  </section>
+
+<section class="form-container">
+
+<?php
+if (isset($message)) {
+   foreach ($message as $msg) {
+      echo '<div class="message">
+      <span>'.$msg.'</span>
+      <i class="bi bi-x-circle" onclick="this.parentElement.remove()"></i>
+      </div>';
+   }
+}
+?>
+
+<form action="" method="POST">
+  <h3>Sign Up</h3>
+  <input type="text" name="name" placeholder="name" required>
+  <input type="email" name="email" placeholder="Email" required>
+  <input type="password" name="password" placeholder="Password" required>
+  <input type="password" name="c_password" placeholder="Confirm Password" required>
+  <input type="submit" name="submit-btn" class="btn" value="Sign Up">
+  <p>Already have an account? <a href="login.php">Log in</a></p>
+</form>
+
+</section>
+
 </body>
 </html>
