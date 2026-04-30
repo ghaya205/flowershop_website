673c18a053be2931ce0e72174fba183644cddbc8
ghaya <ghaya.bedoui.2000@gmail.com>
2026-04-30 19:14:52 +0100

    update database

--- sql/flowershop_db (1).sql
+++ sql/flowershop_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2026 at 05:29 PM
+-- Generation Time: Apr 30, 2026 at 08:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -59,7 +59,7 @@ CREATE TABLE `message` (
 --
 
 CREATE TABLE `orders` (
-  `id` int(255) NOT NULL,
+  `id` int(11) NOT NULL,
   `user_id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `number` varchar(255) NOT NULL,
@@ -71,6 +71,13 @@ CREATE TABLE `orders` (
   `placed_on` varchar(255) NOT NULL,
   `payment_status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(2, 5, 'nour', '96587465', 'nour@gmail.com', 'cash on delevry', '5 street 5', 'red flower(2)', '1110', '09/09/2026', 'completed');
 
 -- --------------------------------------------------------
 
@@ -85,6 +92,18 @@ CREATE TABLE `products` (
   `product_detail` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `product_detail`, `image`) VALUES
+(14, 'red flower', '555', 'dfvdsfsdfsdqdqs', '69f273ff6debb.jpg'),
+(21, ' Endless Kisses - Long Stemmed Roses', '30', 'One dozen glorious red roses, history\'s most passionate flower, beautifully arranged in a stunning red vase accented with an expandable XOXO bracelet. It\'s three impressive gifts in one, which means you\'re in for a lot of XOXOs.\r\nDepending on availability', '69f399556bb24.jpg'),
+(22, 'Twilight Harmony Rose Hatbox', '60', '\"A romantic harmony of lavender and ivory roses arranged in an elegant tiered hatbox, this piece radiates sophistication and calm. Each bloom is handpicked for its velvety texture and gentle hue, making it an enchanting choice for heartfelt occasions. Wra', '69f399af57eae.jpg'),
+(23, 'Graceful Charm Roses', '35', 'An elegant bouquet combining lavender roses, cream roses, and sprigs of lavender flowers. The harmonious blend of colors and textures creates a luxurious floral gift suited for refined occasions, beautifully offered through flower shop Abu Dhabi. Dependin', '69f39a2909653.jpg'),
+(24, 'Lost in Paradise Arrangement', '37', 'A stunning combination of pink lilies and roses, this luxurious bouquet is set to impress with its vibrant yet elegant appeal. Designed with the grace and timeless look admired in Dubai flowers, the arrangement in a clear vase evokes a sense of calm and j', '69f39a766462f.jpg'),
+(25, 'Summer Sunrise Bliss', '36', 'A vibrant mix of golden lilies, sunny gerbera daisies, and fresh purple chrysanthemums, all nestled in a cheerful yellow vase. This bouquet radiates joy and energy, bringing the warmth and positivity of a summer morning to any space. It\'s perfect for brig', '69f39adfcfb11.jpg');
 
 -- --------------------------------------------------------
 
@@ -107,7 +126,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 (3, 'ghayabedoui', 'ghaya@gmail.com', '123456789', 'admin'),
 (5, 'nour', 'nour@gmail.com', '123456789', 'user'),
-(6, 'emna', 'emna@gmail.com', '123456789', 'user');
+(6, 'emna', 'emna@gmail.com', '123456789', 'user'),
+(7, 'radhia', 'radhia@gmail.com', '123456789', 'user'),
+(8, 'red flower', 'red@gmail.com', '123456789', 'user');
 
 -- --------------------------------------------------------
 
@@ -138,6 +159,12 @@ ALTER TABLE `cart`
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -175,16 +202,22 @@ ALTER TABLE `message`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
